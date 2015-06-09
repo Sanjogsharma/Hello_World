02bc6e5256a06cdbc6301aebcf6cc4e44d2376c7
Sanjog Sharma <ss3ev@virginia.edu>
2015-06-08 23:56:16 -0400

    ok

--- data/verify.py
+++ data/verify.py
@@ -1,8 +1,42 @@
-f = open('chipotle.tsv')
-
-chipotle = f.read()
-
-print chipotle
+import csv
+with open('airlines.csv', 'rU') as f:
+    data = [row for row in csv.reader(f)]
 
 
-print type(chipotle)
+header = data[0]
+data = data[1:]
+
+
+#code for problem #1
+
+
+list_incidents = [(float(row[2])+float(row[5]))/30 for row in data]
+
+
+# create empty ist 
+
+#determine the last character
+# if* append the string 
+
+#[x+1 if x >= 45 else x+5 for x in l]
+
+airline_name =[row[0] if row[0][-1] != '*' else row[0][0:-1] for row in data]
+
+
+star_indicator =[0 if row[0][-1] != '*' else 1 for row in data]
+
+
+
+
+'''number 3
+
+> if * add 1, else add 0
+'''
+
+#d = {key: value for (key, value) in iterable}
+
+dummy_dict = {row[0]if row[0][-1] != '*' else row[0][0:-1]: (float(row[2])+float(row[5]))/30 for row in data}
+
+
+
+
